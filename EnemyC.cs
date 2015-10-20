41eee86da86d2d310fb4122945b015bc65349b64
takahashiN <ntaka0323@gmail.com>
2015-10-20 14:18:01 +0900

    動きのパターン分スクリプト追加

---
+++ EnemyC.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyC : MonoBehaviour {
+
+
+
+	private float speed = 10f;
+	private float rotationSmooth = 1f;
+	
+	private Vector3 targetPosition;
+	
+	private float changeTargetSqrDistance = 6f;
+	
+	private void Start()
+	{
+
+		targetPosition = GetRandomPositionOnLevel();
+	}
+	
+	private void Update()
+	{
+
+		// 目標地点との距離が小さければ、次のランダムな目標地点を設定する
+		float sqrDistanceToTarget = Vector3.SqrMagnitude(transform.position - targetPosition);
+		if (sqrDistanceToTarget < changeTargetSqrDistance)
+		{
+			targetPosition = GetRandomPositionOnLevel();
+		}
+		
+		// 目標地点の方向を向く
+		Quaternion targetRotation = Quaternion.LookRotation(targetPosition - transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSmooth);
+		
+		// 前方に進む
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+	
+	public Vector3 GetRandomPositionOnLevel()
+	{
+		float levelSize = 8f;
+		return new Vector3(Random.Range(-levelSize, levelSize), 0, Random.Range(-levelSize, levelSize));
+	}
+}
